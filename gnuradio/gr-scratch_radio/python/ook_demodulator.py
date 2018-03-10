a5f424a2bd8718410617e0c89fe778d576ebd5dc
zynaptic <chris@zynaptic.com>
2018-03-10 14:43:38 +0000

    OOK demodulator sample and offset averaging windows no longer overlap.

--- gnuradio/gr-scratch_radio/python/ook_demodulator.py
+++ gnuradio/gr-scratch_radio/python/ook_demodulator.py
@@ -1,23 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# 
+#
 # Copyright 2018 <+YOU OR YOUR COMPANY+>.
-# 
+#
 # This is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 3, or (at your option)
 # any later version.
-# 
+#
 # This software is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this software; see the file COPYING.  If not, write to
 # the Free Software Foundation, Inc., 51 Franklin Street,
 # Boston, MA 02110-1301, USA.
-# 
+#
 
 import numpy
 from gnuradio import gr
@@ -35,23 +35,25 @@ class ook_demodulator(gr.sync_block):
         self.offsetAvgPeriod = self.symbolAvgPeriod * 10
         self.symbolAccValue = 0.0
         self.offsetAccValue = 0.0
-        self.set_history(self.offsetAvgPeriod + 1)
+        self.set_history(self.symbolAvgPeriod + self.offsetAvgPeriod + 1)
 
     def work(self, input_items, output_items):
         in0 = input_items[0]
         out = output_items[0]
-        for i in range(self.offsetAvgPeriod, len(in0)):
-			oldSampleIndex = i - self.symbolAvgPeriod
-			oldOffsetIndex = i - self.offsetAvgPeriod
-			absInputSample = abs(in0[i])
-			absOldSymbolSample = abs(in0[oldSampleIndex])
-			absOldOffsetSample = abs(in0[oldOffsetIndex])
-			
-			self.symbolAccValue += absInputSample - absOldSymbolSample
-			self.offsetAccValue += absInputSample - absOldOffsetSample
-			
-			symbolValue = self.symbolAccValue / self.symbolAvgPeriod
-			symbolValue -= self.offsetAccValue / self.offsetAvgPeriod 
-			out[i - self.offsetAvgPeriod] = symbolValue
-			
+        for i in range(len(in0) - (self.symbolAvgPeriod + self.offsetAvgPeriod)):
+            oldOffsetIndex = i
+            oldSampleIndex = oldOffsetIndex + self.offsetAvgPeriod
+            newSampleIndex = oldSampleIndex + self.symbolAvgPeriod
+
+            absInputSample = abs(in0[newSampleIndex])
+            absOldSymbolSample = abs(in0[oldSampleIndex])
+            absOldOffsetSample = abs(in0[oldOffsetIndex])
+
+            self.symbolAccValue += absInputSample - absOldSymbolSample
+            self.offsetAccValue += absOldSymbolSample - absOldOffsetSample
+
+            symbolValue = self.symbolAccValue / self.symbolAvgPeriod
+            symbolValue -= self.offsetAccValue / self.offsetAvgPeriod
+            out[i] = symbolValue
+
         return len(out)
